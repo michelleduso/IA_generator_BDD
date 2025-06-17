abb7c24ede4d29cc00ce9e981b73ddccb64fad5d
Michelle Duso <chelleduso@gmail.com>
2025-06-17 17:51:16 -0300

    openIA model configuration

--- bdd_generator.py
+++ bdd_generator.py
@@ -7,3 +7,9 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 OPENAI_KEY = os.getenv("API_KEY")
 
+chat = ChatOpenAI(
+    model_name="gpt-4o-mini",
+    openai_api_key=OPENAI_KEY,
+    temperature=0.3
+)
+
